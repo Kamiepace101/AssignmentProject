a788ec659cf06608ff82c4128825187bc9f62d5b
Kamiepace101 <74590891+Kamiepace101@users.noreply.github.com>
2021-01-10 17:20:28 +0100

    Change of position and Scale
    Changed the scale of the box and made them into a rectangle.
    Made the rectangle drop from the screen.

---
+++ Magic Tiles 3/Assets/Scripts/BlueSquare.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueSquare : MonoBehaviour
+{
+    void Update () {
+        
+        if (transform.position.y >= -8.5)
+    {
+        transform.Translate(0f, -0.25f, 0f);
+    }
+    
+    }
+}
