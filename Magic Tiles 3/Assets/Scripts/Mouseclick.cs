eb4971d149f305e7f1296d039ca44d0144d873c8
Kamiepace101 <74590891+Kamiepace101@users.noreply.github.com>
2021-01-12 12:53:51 +0100

    Speed and Destroing Rectangles
    Added speed to the Rectangle by using Time.deltatime
    Made the click mouse button work
    Added destroy when one click on the rectangles.

--- Magic Tiles 3/Assets/Scripts/Mouseclick.cs
+++ Magic Tiles 3/Assets/Scripts/Mouseclick.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Mouseclick : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Destroy(gameObject,3f);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        //use left mouse buttton to make the blue boxes vanish
+        //we are going to use local variable
+        //the button destroy represents the left mouse down button 
+        //bool pressedDestroyButton = Input.GetMouseButtonDown();
         
+
+        //if pressedDestroyButton is true which means we are pressing Left mouse button
+        //make the boxes vanish
+        //if the pressedDestroyButton = true
+        // if(pressedDestroyButton)
+        // {
+        //     Debug.Log("You pressed the left mouse button");
+        // }
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+
+
+    private void OnMouseDown() 
+    {
+        Debug.Log("You clicked the tile");
+        Destroy(gameObject);
     }
 }
+
